18db52a6a653bbfee7a8cba20960efb6e163ad38
Nicolas Benoit <123339751+Nikandro1@users.noreply.github.com>
2024-08-20 19:01:31 -0300

    agregando configuracion 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://nikandro1.github.io/RHCP/',
+  base: 'https://github.com/Nikandro1/RHCP',
 })
