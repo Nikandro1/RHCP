0ff4a313b3344af44cbb1eabad8fa745d9cda557
Nicolas Benoit <123339751+Nikandro1@users.noreply.github.com>
2024-08-20 22:26:07 -0300

    agregando configuracion 6

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Routes>
-    <Route path="/" element={<App />} />
-  </Routes>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </Router>
 )
